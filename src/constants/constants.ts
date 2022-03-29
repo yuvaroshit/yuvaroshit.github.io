e64e87b436d715f80b9ad12d1c2e0608680bbd47
yuvaroshit <yuvaroshit@gmail.com>
2022-03-29 16:12:05 -0400

    WalmartFE deployment

--- src/constants/constants.ts
+++
@@ -1,8 +0,0 @@
-export class Constants {
-  public static baseURL = 'https://jsonplaceholder.typicode.com';
-  public static users = '/users';
-  public static posts = '/posts';
-  public static comments = '/comments';
-  public static albums = '/albums';
-  public static photos = '/photos';
-}
--- src/constants/globals.ts
+++
@@ -1,5 +0,0 @@
-export class GlobalFunctions {
-  public static generateParams(filter: any): string {
-    return Object.keys(filter).map(key => key + '=' + filter[key]).join('&');;
-  }
-}
